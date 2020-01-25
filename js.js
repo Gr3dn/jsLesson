bde90b94d61efefb3f439300922c69192b91ccfc
Gr3dn <60060912+Gr3dn@users.noreply.github.com>
2020-01-26 00:14:50 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+var budget = prompt('Ваш бюждет?');
+var name =  prompt('Название вашего магазина?');
+var shopGoods1 = prompt('Какой тип товаров будем продавать?');
+var shopGoods2 = prompt('Какой тип товаров будем продавать?');
+var shopGoods3 = prompt('Какой тип товаров будем продавать?');
+ var mainList = {
+	budget : budget,
+	name : name,
+	shopGoods : [shopGoods1, shopGoods2, shopGoods3],
+	employers : {},
+}
+alert('бюждет на 1 день составит ' + budget/30 + " " + 'dollars');# Daniil
