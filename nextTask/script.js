6f6375f84d0e04fbb7861f1c38062e0b08557d30
Gr3dn <60060912+Gr3dn@users.noreply.github.com>
2020-05-24 13:34:59 +0300

    Delete script.js

--- nextTask/script.js
+++
@@ -1,5 +0,0 @@
-var input7 = document.querySelectorAll('input')[7];
-var input8 = document.querySelectorAll('input')[8];
-var input9 = document.querySelectorAll('input')[9];
-var InPutS = [input7, input8, input9];
-console.log(input7, input8, input9);
